cac72a9ddd929122e210bf03591b9a1c36f697ba
Estuardo Aguirre <EstuardoA25@gmail.com>
2020-11-09 12:25:24 -0600

    Pages and Routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>Hello world - changed!!</div>
+    <div>
+        <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+            <nuxt-link :to="`/home/${home.objectID}`" no-prefetch>
+                <HomeCard :home="home"/>
+            </nuxt-link>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import homes from '@/data/homes.json'
 
+export default {
+    head() {
+        return {
+            title: 'Homepage',
+            meta: [{
+                name: 'description',
+                content: 'This is a homepage!',
+                hid: 'description'
+            }]
+        }
+    },
+    data() {
+        return {
+            homes: homes.slice(0, 3)
+        }
+    }
 }
 </script>
